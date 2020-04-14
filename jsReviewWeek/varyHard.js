c71bd118a77fda26ec695c7f62837d2940ddfc0c
Zell Myles 2 <ZellMyles@MacBook-Pro.local>
2020-04-13 22:38:47 -0400

    jsReviewWeekFinal

--- jsReviewWeek/varyHard.js
+++ jsReviewWeek/varyHard.js
@@ -5,14 +5,19 @@
 
 
 
+// Coins of different denominations will go into an array
+// Set target amount
+// Find highest number in array
+// Find lowest number in array
 
 
 
-
-const arr = [10,5,2]
+const arr = [10,5,2,1]    // Can any of these be added up to get our target amount? (10 + 10 + 5 = 25) 3 coins
 
 const amount = 25
 
-fewestNum = (arr, total) => {
+fewestNum = (arr, amount) => {
+    const highNum = math.max(arr)
+    const lowNum = math.min(arr)
     
 }
